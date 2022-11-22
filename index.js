4315c0ee7411bcbb399f5bffcfbcd47983fdb0ce
Ankan Sen <maharajankan@gmail.com>
2022-11-22 09:27:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ window.addEventListener("mousemove", function (e) {
   console.log(canvas.width,canvas.height);
 
 ctx.fillStyle = "white";
-ctx.font = (canvas.width*0.0001) + "px Arial";
+ctx.font ="30px Arial";
 ctx.fillText("Ankan", 0, 25);
 const textcoordinates = ctx.getImageData(0, 0, 100, 100);
 
